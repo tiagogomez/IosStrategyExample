db6aa5655d7b7f32976bc5b977c83ede0e4d96b5
tiagogomez <tiago.gomezg@ggail.com>
2021-09-13 13:10:38 -0500

    Adding UI options to change payment methods

--- IosStrategyExample/Presentation/ViewsClient/PaymentViewController.swift
+++ IosStrategyExample/Presentation/ViewsClient/PaymentViewController.swift
@@ -9,26 +9,78 @@ import UIKit
 
 class PaymentViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     private var order = OrderContext()
-
+    private var selectedStrategy: PayStrategy?
+    private var selectedRow: PaymentMethods?
+    
+    
+    enum PaymentMethods: Int, CaseIterable {
+        
+        case creditCard
+        case payPal
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.isScrollEnabled = false
+    }
+    
+    @IBAction func payButtonPressed(_ sender: Any) {
         /**
          * Client code
          */
-        let payPalStrategy = PayByPayPal()
-        let creditCardStrategy = PayByCreditCard()
         
-        // Choose strategy
         order.setTotalCost(cost: 10000)
         order.setClosed()
-        order.strategy = payPalStrategy
-        order.processOrder()
-        order.payOrder()
-        
-        order.strategy = creditCardStrategy
         order.processOrder()
         order.payOrder()
     }
 }
 
+extension PaymentViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        PaymentMethods.allCases.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        switch indexPath.row {
+        case PaymentMethods.creditCard.rawValue:
+            cell.imageView?.image = UIImage(named: "Visa")
+            cell.textLabel?.text = "Pago Tarjeta"
+            cell.accessoryType = (indexPath.row == selectedRow?.rawValue) ? .checkmark : .none
+        case PaymentMethods.payPal.rawValue:
+            cell.imageView?.image = UIImage(named: "PayPal")
+            cell.textLabel?.text = "Pago PayPal"
+            cell.accessoryType = (indexPath.row == selectedRow?.rawValue) ? .checkmark : .none
+        default:
+            cell.textLabel?.text = "Others"
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedRow = PaymentMethods(rawValue: indexPath.row)
+        switch selectedRow {
+        case .creditCard:
+            selectedStrategy = PayByCreditCard()
+        case .payPal:
+            selectedStrategy = PayByPayPal()
+        default:
+            print("No payment selected")
+        }
+        order.strategy = selectedStrategy
+        tableView.reloadData()
+    }
+    
+}
